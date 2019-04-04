9192e6832e6719030afbbbda71743904b4417565
mgesswein <48776552+mgesswein@users.noreply.github.com>
2019-04-03 21:06:12 -0300

    Add files via upload

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.myapplication
+
+import android.app.Activity
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.view.View
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun clickClickMeButton(view: View){
+        val intent = Intent(this, Main2Activity::class.java)
+        startActivity(intent)
+    }
+
+}
